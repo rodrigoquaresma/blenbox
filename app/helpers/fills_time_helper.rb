348644a05cc214c3a2870e91277c86451898e0f0
rodrigo quaresma <rodrigoquaresma@mpro-quaresma.local>
2015-04-02 09:38:31 -0300

    clean up

--- app/helpers/fills_time_helper.rb
+++ app/helpers/fills_time_helper.rb
@@ -2,16 +2,10 @@ module FillsTimeHelper
 
   def hours_in
     hours_in = Array.new (15) {rand(8..10)}
-    # hours_in.each do |hour_in|
-    #   hour_in
-    # end
   end
 
   def minutes_in
     minutes_in = Array.new (15) {rand(0..59)}
-    # minutes_in.each do |minute_in|
-    #   minute_in
-    # end
   end
 
   def xhour_in
@@ -26,42 +20,8 @@ module FillsTimeHelper
     end
   end
 
-  def time_in
-    hours_in
-  end
-
-
   def time_build(hour_in, minute_in)
-
     return "#{hour_in}:#{minute_in}"
-
-  end
-
-
-  def xtimes_in
-    hours_in.each do |hour_in|
-      hour_in
-    end
-    minutes_in.each do |minute_in|
-      minute_in
-    end
-    # [hours_in],[minutes_in]
-  end
-
-  def time_ger
-
-    # times_in.each do |time_in|
-
-    # end
-
-
-
-
-    # (0..5).each do |i|
-    #    puts "Value of local variable is #{i}"
-    # end
-
-
   end
 
 end
