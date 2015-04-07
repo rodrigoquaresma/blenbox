67a8548cb2deba2b037e738188946cb2e7e7ac78
rodrigo quaresma <rodrigoquaresma@mpro-quaresma.local>
2015-04-07 08:25:53 -0300

    put holidays

--- app/controllers/fills_time_controller.rb
+++ app/controllers/fills_time_controller.rb
@@ -1,5 +1,14 @@
 class FillsTimeController < ApplicationController
   def index
+
+    holiday_1 = Time.new(2015,1,1)
+    holiday_2 = Time.new(2015,2,16)
+    holiday_3 = Time.new(2015,2,17)
+    holiday_4 = Time.new(2015,3,23)
+    holiday_5 = Time.new(2015,4,3)
+    holiday_6 = Time.new(2015,4,21)
+    holiday_7 = Time.new(2015,5,1)
+    
   end
   def jan
   end
