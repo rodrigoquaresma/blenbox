9afb2168c5cf63c5f194b68559ac2a323409812a
rodrigo quaresma <rodrigoquaresma@mpro-quaresma.local>
2015-05-16 11:57:04 -0300

    fix navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def active_tab(item)
+    # controller.split("/").last == item ? 'active': ''
+    # controller.split("/").first == item ? 'active': ''
+    'active' if params[:controller].include?(item)
+  end
+
 end
