235e3e4c304ea9162607194c10ef13d3af154e9b
rodrigo quaresma <rodrigoquaresma@mpro-quaresma.local>
2015-05-23 00:29:31 -0300

    add breadcrumb

--- app/controllers/ux_calculators_controller.rb
+++ app/controllers/ux_calculators_controller.rb
@@ -4,6 +4,9 @@ class UxCalculatorsController < ApplicationController
   end
 
   def cn
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous']
+    ]
     @ask = 'Comparing data?'
     @ask_help = ''
     @answer_y = 'cn_cd_y'
@@ -11,13 +14,22 @@ class UxCalculatorsController < ApplicationController
     render 'cn'
   end
   def cn_cd_y
-    @ask = 'Diferent users in each group?'
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','yes']
+    ]
+    @ask = 'Different users in each group?'
     @ask_help = ''
     @answer_y = 'cn_cd_y_du_y'
     @answer_n = 'cn_cd_y_du_n'
     render 'cn'
   end
   def cn_cd_y_du_y
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','yes'],
+      ['cn_cd_y','Different users in each group?','yes']
+    ]
     @ask = '3 or more groups?'
     @ask_help = ''
     @answer_y = 'cn_cd_y_du_y_mg_y'
@@ -25,15 +37,32 @@ class UxCalculatorsController < ApplicationController
     render 'cn'
   end
   def cn_cd_y_du_y_mg_y
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','yes'],
+      ['cn_cd_y','Different users in each group?','yes'],
+      ['cn_cd_y_du_y','3 or more groups?','yes']
+    ]
     @formula = "A - ANOVA"
     render 'show_formula'
   end
   def cn_cd_y_du_y_mg_n
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','yes'],
+      ['cn_cd_y','Different users in each group?','yes'],
+      ['cn_cd_y_du_y','3 or more groups?','no']
+    ]
     @formula = "2t - 2-sample t"
     render 'show_formula'
   end
 
   def cn_cd_y_du_n
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','yes'],
+      ['cn_cd_y','Different users in each group?','no']
+    ]
     @ask = '3 or more groups?'
     @ask_help = ''
     @answer_y = 'cn_cd_y_du_n_mg_y'
@@ -41,15 +70,31 @@ class UxCalculatorsController < ApplicationController
     render 'cn'
   end
   def cn_cd_y_du_n_mg_y
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','yes'],
+      ['cn_cd_y','Different users in each group?','no'],
+      ['cn_cd_y_du_n','3 or more groups?','yes']
+    ]
     @formula = "Arm - Within subjects ANOVA"
     render 'show_formula'
   end
   def cn_cd_y_du_n_mg_n
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','yes'],
+      ['cn_cd_y','Different users in each group?','no'],
+      ['cn_cd_y_du_n','3 or more groups?','no']
+    ]
     @formula = "tt - paired t"
     render 'show_formula'
   end
 
   def cn_cd_n
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','no']
+    ]
     @ask = 'Testing against a benchmark?'
     @ask_help = ''
     @answer_y = 'cn_cd_n_ab_y'
@@ -57,6 +102,11 @@ class UxCalculatorsController < ApplicationController
     render 'cn'
   end
   def cn_cd_n_ab_y
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','no'],
+      ['cn_cd_n','Testing against a benchmark?','yes']
+    ]
     @ask = 'Task time?'
     @ask_help = ''
     @answer_y = 'cn_cd_n_ab_y_tt_y'
@@ -64,15 +114,32 @@ class UxCalculatorsController < ApplicationController
     render 'cn'
   end
   def cn_cd_n_ab_y_tt_y
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','no'],
+      ['cn_cd_n','Testing against a benchmark?','yes'],
+      ['cn_cd_n_ab_y','Task time?','yes']
+    ]
     @formula = "tlog - 1-Sample t (log)"
     render 'show_formula'
   end
   def cn_cd_n_ab_y_tt_n
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','no'],
+      ['cn_cd_n','Testing against a benchmark?','yes'],
+      ['cn_cd_n_ab_n','Task time?','no']
+    ]
     @formula = "t - 1-Sample t"
     render 'show_formula'
   end
 
   def cn_cd_n_ab_n
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','no'],
+      ['cn_cd_n','Testing against a benchmark?','no']
+    ]
     @ask = 'Task time?'
     @ask_help = ''
     @answer_y = 'cn_cd_n_ab_n_tt_y'
@@ -80,15 +147,30 @@ class UxCalculatorsController < ApplicationController
     render 'cn'
   end
   def cn_cd_n_ab_n_tt_y
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','no'],
+      ['cn_cd_n','Testing against a benchmark?','no'],
+      ['cn_cd_n_ab_n','Task time?','yes']
+    ]
     @formula = "tlog - t (LOG) conf. interval"
     render 'show_formula'
   end
   def cn_cd_n_ab_n_tt_n
+    @breadcrumbs = [
+      ['index','What type of Data?','continuous'],
+      ['cn','Comparing data?','no'],
+      ['cn_cd_n','Testing against a benchmark?','no'],
+      ['cn_cd_n_ab_n','Task time?','no']
+    ]
     @formula = "t - t conf. interval"
     render 'show_formula'
   end
 
   def bn
+    @breadcrumbs = [
+      ['index','What type of Data?','binary']
+    ]
     @ask = 'Comparing groups?'
     @ask_help = ''
     @answer_y = 'bn_cg_y'
@@ -96,6 +178,10 @@ class UxCalculatorsController < ApplicationController
     render 'bn'
   end
   def bn_cg_y
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','yes']
+    ]
     @ask = 'Different users in each group?'
     @ask_help = ''
     @answer_y = 'bn_cg_y_du_y'
@@ -103,6 +189,11 @@ class UxCalculatorsController < ApplicationController
     render 'bn'
   end
   def bn_cg_y_du_y
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','yes'],
+      ['bn_cg_y','Different users in each group?','yes']
+    ]
     @ask = '3 or more groups?'
     @ask_help = ''
     @answer_y = 'bn_cg_y_du_y_mg_y'
@@ -110,6 +201,12 @@ class UxCalculatorsController < ApplicationController
     render 'bn'
   end
   def bn_cg_y_du_y_mg_y
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','yes'],
+      ['bn_cg_y','Different users in each group?','yes'],
+      ['bn_cg_y_du_y','3 or more groups?','yes']
+    ]
     @ask = 'Large sample?'
     @ask_help = ''
     @answer_y = 'bn_cg_y_du_y_mg_y_ls_y'
@@ -117,19 +214,44 @@ class UxCalculatorsController < ApplicationController
     render 'bn'
   end
   def bn_cg_y_du_y_mg_y_ls_y
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','yes'],
+      ['bn_cg_y','Different users in each group?','yes'],
+      ['bn_cg_y_du_y','3 or more groups?','yes'],
+      ['bn_cg_y_du_y_mg_y','large sample?','yes']
+    ]
     @formula = "chi-square"
     render 'show_formula'
   end
   def bn_cg_y_du_y_mg_y_ls_n
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','yes'],
+      ['bn_cg_y','Different users in each group?','yes'],
+      ['bn_cg_y_du_y','3 or more groups?','yes'],
+      ['bn_cg_y_du_y_mg_y','large sample?','no']
+    ]
     @formula = "2p - N-1 two proportion test on pairs"
     render 'show_formula'
   end
   def bn_cg_y_du_y_mg_n
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','yes'],
+      ['bn_cg_y','Different users in each group?','yes'],
+      ['bn_cg_y_du_y','3 or more groups?','no']
+    ]
     @formula = "2p - N-1 two proportion test"
     render 'show_formula'
   end
 
   def bn_cg_y_du_n
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','yes'],
+      ['bn_cg_y','Different users in each group?','no']
+    ]
     @ask = '3 or more groups?'
     @ask_help = ''
     @answer_y = 'bn_cg_y_du_n_mg_y'
@@ -137,15 +259,31 @@ class UxCalculatorsController < ApplicationController
     render 'bn'
   end
   def bn_cg_y_du_n_mg_y
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','yes'],
+      ['bn_cg_y','Different users in each group?','no'],
+      ['bn_cg_y_du_n','3 or more groups?','yes']
+    ]
     @formula = "pp - McNemher Exact Test using CI"
     render 'show_formula'
   end
   def bn_cg_y_du_n_mg_n
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','yes'],
+      ['bn_cg_y','Different users in each group?','no'],
+      ['bn_cg_y_du_n','3 or more groups?','no']
+    ]
     @formula = "pp - McNemher Exact Test"
     render 'show_formula'
   end
 
   def bn_cg_n
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','no']
+    ]
     @ask = 'Testing against a benchmark?'
     @ask_help = ''
     @answer_y = 'bn_cg_n_ab_y'
@@ -153,10 +291,20 @@ class UxCalculatorsController < ApplicationController
     render 'bn'
   end
   def bn_cg_n_ab_y
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','no'],
+      ['bn_cg_n','Testing against a benchmark?','yes']
+    ]
     @formula = "Pb - 1-Sample Binomial"
     render 'show_formula'
   end
   def bn_cg_n_ab_n
+    @breadcrumbs = [
+      ['index','What type of Data?','binary'],
+      ['bn','Comparing groups?','no'],
+      ['bn_cg_n','Testing against a benchmark?','no']
+    ]
     @formula = "Pb - Adj. Wald CI"
     render 'show_formula'
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
 
   def active_tab(item)
-    # controller.split("/").last == item ? 'active': ''
-    # controller.split("/").first == item ? 'active': ''
     'active' if params[:controller].include?(item)
   end
 
