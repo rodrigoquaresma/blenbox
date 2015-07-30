d2ad837b9773f12f6695f490386dc91b760613b0
rodrigo quaresma <rodrigoquaresma@mpro-quaresma.local>
2015-07-30 07:52:11 -0300

    building

--- app/controllers/bbq_controller.rb
+++ app/controllers/bbq_controller.rb
@@ -1,5 +1,7 @@
 class BbqController < ApplicationController
   include ActionView::Helpers::NumberHelper
+  # include ActionView::Helpers::BbqHelper
+  include ActionView::Base::BbqHelper
 
   def index
   end
@@ -10,16 +12,16 @@ class BbqController < ApplicationController
     wgt_each_kids = 0.5
 
     qtd_men = 10
-    qtd_women = 5
-    qtd_kids = 2
+    qtd_women = 8
+    qtd_kids = 6
 
-    val_bovine = 0.6
+    val_bovine = 0.7
     val_swine = 0.2
-    val_chicken = 0.2
+    val_chicken = 0.1
 
-    qtd_cut_bovine = 4
-    qtd_cut_swine = 3
-    qtd_cut_chicken = 2
+    @qtd_cut_bovine = 4
+    @qtd_cut_swine = 2
+    @qtd_cut_chicken = 2
 
     wgt_meats = (wgt_each_men*qtd_men)+(wgt_each_women*qtd_women)+(wgt_each_kids*qtd_kids)
 
@@ -27,9 +29,40 @@ class BbqController < ApplicationController
     @wgt_swine = wgt_meats*val_swine
     @wgt_chicken = wgt_meats*val_chicken
 
-    @wgt_each_cut_bovine = @wgt_bovine/qtd_cut_bovine
-    @wgt_each_cut_swine = @wgt_swine/qtd_cut_swine
-    @wgt_each_cut_chicken = @wgt_chicken/qtd_cut_chicken
+    @wgt_each_cut_bovine = @wgt_bovine/@qtd_cut_bovine
+    @wgt_each_cut_swine = @wgt_swine/@qtd_cut_swine
+    @wgt_each_cut_chicken = @wgt_chicken/@qtd_cut_chicken
+
+    xyz = 0
+
+    meats.each do |xmeat|
+
+      if xmeat[0] == 'bovina (boi)'
+        wgt_meat_lim = @wgt_bovine
+        wgt_each_cut = @wgt_each_cut_bovine
+        qtd_cuts_lim = @qtd_cut_bovine
+      elsif xmeat[0] == 'suína (porco)'
+        wgt_meat_lim = @wgt_swine
+        wgt_each_cut = @wgt_each_cut_swine
+        qtd_cuts_lim = @qtd_cut_swine
+      elsif xmeat[0] == 'aves (frango)'
+        wgt_meat_lim = @wgt_chicken
+        wgt_each_cut = @wgt_each_cut_chicken
+        qtd_cuts_lim = @qtd_cut_chicken
+      end
+
+      i = 0
+
+      xmeat[1].each do |xcut_meat|
+        while i < qtd_cuts_lim  do
+          xyz +=1
+          i +=1
+        end
+      end
+
+    end
+
+    @showme = xyz
 
     render :action => :result
   end
--- app/helpers/bbq_helper.rb
+++ app/helpers/bbq_helper.rb
@@ -1,8 +1,15 @@
 module BbqHelper
 
-  def min_value_cut(wgt_cut,wgt_cut_min)
+  def min_value_cut(wgt_cut,wgt_cut_min,wgt_cut_max)
     if wgt_cut <= wgt_cut_min
+      # wgt_cut_minimo = wgt_cut_min
       return wgt_cut_min
+    # elsif wgt_cut >= (wgt_cut_min*wgt_cut_max)
+    #   wgt_cut_maximo = wgt_cut_min*wgt_cut_max
+    #   return wgt_cut_maximo
+    # else
+    #   wgt_cut_medium = (wgt_cut_min + (wgt_cut_min*wgt_cut_max)) / 2
+    #   return wgt_cut_medium
     else
       return wgt_cut
     end
@@ -24,41 +31,40 @@ module BbqHelper
     [
       ['bovina (boi)',
         [
-          ['costela',1.2,3,14],
-          ['alcatra',0.8,0.5,20],
-          ['vazio',0.7,0.5,20],
-          ['fraldinha',0.7,0.5,18],
-          ['entrecot/chuleta/bisteca',0.7,0.5,22],
-          ['contrafilé/bife de chorizo',0.8,0.5,21],
-          ['picanha',0.8,0.5,32],
-          ['maminha',0.7,0.5,23],
-          ['cupim',1.2,0.5,20]
+          ['costela',1.2,3,14,5],
+          ['vazio',0.7,1.2,20,3],
+          ['alcatra',0.8,1.3,20,1],
+          ['fraldinha',0.7,1.5,18,3],
+          ['entrecot/chuleta/bisteca',0.4,1.4,22,3],
+          ['contrafilé/bife de chorizo',0.4,1.5,21,3],
+          ['picanha',0.8,1.3,32,1],
+          ['maminha',0.7,1.4,23,3],
+          ['cupim',1.2,0.5,20,3]
         ]
       ],
       ['suína (porco)',
         [
-          ['costela',1.200,1,17],
-          ['linguiça toscana',1.200,0.2,10],
-          ['barriga',1.200,1,14],
-          ['pernil',1.200,0.8,15],
-          ['lombo',1.200,0.7,16],
-          ['carré/bisteca',1.200,0.5,17],
-          ['alcatra',1.200,0.6,19],
-          ['picanha',1.200,0.7,21],
-          ['filé mignon',1.200,0.7,22]
+          ['costela',1.200,1.8,17,3],
+          ['linguiça toscana',1.200,10,10,5],
+          ['barriga',1.100,1.6,14,3],
+          ['pernil',1.200,1.4,15,3],
+          ['lombo',1.200,1.3,16,3],
+          ['carré/bisteca',1.200,2.0,17,1],
+          ['alcatra',1.000,1.6,19,1],
+          ['picanha',0.800,1.4,21,1],
+          ['filé mignon',0.600,1.8,22,1]
         ]
       ],
       ['aves (frango)',
         [
-          ['coxa',1.200,0.3,7],
-          ['coxa com sobrecoxa',1.200,0.4,6],
-          ['asinha',1.200,0.2,10],
-          ['linguiça de frango',1.200,0.25,14],
-          ['coração de frango',1.200,0.15,18]
+          ['coxa',0.200,9.0,7,3],
+          ['coxa com sobrecoxa',0.200,0.4,6,3],
+          ['asinha de frango',1.200,0.2,10,3],
+          ['linguiça de frango',1.200,0.25,14,5],
+          ['coração de frango',1.200,0.15,18,1]
         ]
       ]
     ]
-
   end
 
 end
